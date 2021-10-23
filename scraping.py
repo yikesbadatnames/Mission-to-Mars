92db5853c7126a1642b6027423119de17878d7f7
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2021-10-23 14:31:31 -0700

    fixed variable name

--- scraping.py
+++ scraping.py
@@ -19,11 +19,11 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres" : hemispheres(browser)
     }
 
     # Stop webdriver and return data
-    browser.quit()
     return data
 
 
@@ -97,7 +97,32 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
+def hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    #executable_path = {'executable_path': ChromeDriverManager().install()}
+    #browser = Browser('chrome', **executable_path, headless=False)
+    url = 'https://marshemispheres.com/'
+    browser.visit(url+"index.html")
+    # getting hemispheres
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    element = browser.find_by_css("a.product-item img") #we find the a tag, and then pair the link with the img link nested in the a tag.
+    (len(element))
+    for item in range((len(element))):
+        browser.find_by_css("a.product-item img")[item].click()
+        hemi_dict = {}
+        sample = browser.find_by_text("Sample").first
+        hemi_dict["img_url"] = sample["href"]
+        hemi_dict["title"] =  browser.find_by_css("h2.title").text
+        hemisphere_image_urls.append(hemi_dict)
+        browser.back()
+        print(hemi_dict["img_url"])
+    return hemisphere_image_urls
+
 if __name__ == "__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
+
